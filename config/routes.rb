e0221a8031d538c466bc31c27db13300306337cc
Skanner33 <mathewmageto@gmail.com>
2023-03-23 12:36:48 +0300

    created todo endpoint

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  #user
+  post '/register', to: "users#register"
+  post '/users/login', to: "users#login"
+  delete '/users/logout', to: "users#logout"
+  get '/users/login/check', to: "users#check_login_status"
+
+  #todos
+  post '/todos', to: 'todos#create'
+
+end
